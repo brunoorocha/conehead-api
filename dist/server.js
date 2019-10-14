73bdee2af72813715511d34e540a18528eee81ea
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-14 11:29:15 -0300

    Running yarn build command

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,5 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./application/app'); var _app2 = _interopRequireDefault(_app);
 
-_app2.default.listen(3333)
+const port = process.env.PORT || 3333
+_app2.default.listen(port)
 console.log('Conehead API is ready!')
