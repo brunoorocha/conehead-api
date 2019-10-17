fe66ac7ec14738b40019fcaee3fe4de3c52e5449
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-16 21:40:35 -0300

    Deleting build folder

--- dist/workers/measurement/RemoveMeasurementWithId.js
+++
@@ -1,9 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }Object.defineProperty(exports, "__esModule", {value: true});
-
-
-const removeMeasurementWithId = async (measurementId, measurementStore) => {
-  const measurement = await measurementStore.remove(measurementId)
-  return measurement
-}
-
-exports. default = removeMeasurementWithId
