30703094a06db737949cd503a0a4193d9bc4399c
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-17 09:48:28 -0300

    Some changes in start command and in Procfile

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./application/app'); var _app2 = _interopRequireDefault(_app);
-
-const port = process.env.PORT || 3333
-_app2.default.listen(port)
-console.log(`Conehead API is ready and listen on port ${port}!`)
--- dist/workers/measurement/ListMeasurements.js
+++
@@ -1,9 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }Object.defineProperty(exports, "__esModule", {value: true});
-
-
-const listMeasurements = async (measurementStore) => {
-  const measurements = await measurementStore.fetchAll()
-  return measurements
-}
-
-exports. default = listMeasurements
