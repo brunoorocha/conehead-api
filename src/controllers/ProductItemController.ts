5ba3be3fb16194e6a70b1d382588120cfa92ca36
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-18 09:35:01 -0300

    Added request body/params validation for product item routes and using the RequestValidationCheck worker to tell to controller if there's some error

--- src/controllers/ProductItemController.ts
+++ src/controllers/ProductItemController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express'
 import CreateProductItemWorker from '../workers/product-item/CreateProductItem'
 import ListProductItemsOfProductWorker from '../workers/product-item/ListProductItemsOfProduct'
 import GetProductItemWithIdWorker from '../workers/product-item/GetProductItemWithId'
+import RequestValidationCheckWorker, { ResponseError } from '../workers/requestValidationCheck'
 
 class ProductItemController {
   public productItemStore: ProductItemStore
@@ -13,25 +14,52 @@ class ProductItemController {
   }
 
   public store = async (req: Request, res: Response): Promise<Response> => {
-    const productId: string = req.params.productId
-    const quantity: number = req.body.quantity
-    const price: number = req.body.price
-    const expiration: string = req.body.expiration
+    try {
+      await RequestValidationCheckWorker(req)
+      const productId: string = req.params.productId
+      const quantity: number = req.body.quantity
+      const price: number = req.body.price
+      const expiration: string = req.body.expiration
 
-    const product = await CreateProductItemWorker(quantity, price, expiration, productId, this.productItemStore)
-    return res.json(product)
+      const product = await CreateProductItemWorker(quantity, price, expiration, productId, this.productItemStore)
+      return res.json(product)
+    } catch (error) {
+      if ((error as ResponseError).status) {
+        return res.status(error.status).json({ errors: error.errors })
+      }
+
+      return res.status(500).json({ error })
+    }
   }
 
   public index = async (req: Request, res: Response): Promise<Response> => {
-    const productId: string = req.params.productId
-    const productItems = await ListProductItemsOfProductWorker(productId, this.productItemStore)
-    return res.json(productItems)
+    try {
+      await RequestValidationCheckWorker(req)
+      const productId: string = req.params.productId
+      const productItems = await ListProductItemsOfProductWorker(productId, this.productItemStore)
+      return res.json(productItems)
+    } catch (error) {
+      if ((error as ResponseError).status) {
+        return res.status(error.status).json({ errors: error.errors })
+      }
+
+      return res.status(500).json({ error })
+    }
   }
 
   public get = async (req: Request, res: Response): Promise<Response> => {
-    const productItemId: string = req.params.productItemId
-    const productItem = await GetProductItemWithIdWorker(productItemId, this.productItemStore)
-    return res.json(productItem)
+    try {
+      await RequestValidationCheckWorker(req)
+      const productItemId: string = req.params.productItemId
+      const productItem = await GetProductItemWithIdWorker(productItemId, this.productItemStore)
+      return res.json(productItem)
+    } catch (error) {
+      if ((error as ResponseError).status) {
+        return res.status(error.status).json({ errors: error.errors })
+      }
+
+      return res.status(500).json({ error })
+    }
   }
 }
 
--- src/routes/product-item/ProductItemRoutes.ts
+++ src/routes/product-item/ProductItemRoutes.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import ProductItemController from '../../controllers/ProductItemController'
+import { check } from 'express-validator'
 
 const productItemRoutes = express.Router()
-// productRoutes.get('/products', ProductController.index)
-productItemRoutes.get('/products/:productId/items', ProductItemController.index)
-productItemRoutes.get('/products/:productId/items/:productItemId', ProductItemController.get)
-productItemRoutes.post('/products/:productId/items', ProductItemController.store)
-// productRoutes.get('/products/:productId', ProductController.get)
-// productRoutes.delete('/products/:productId', ProductController.remove)
+productItemRoutes.get('/products/:productId/items', [check('productId').isMongoId()], ProductItemController.index)
+productItemRoutes.get('/products/:productId/items/:productItemId', [check('productId').isMongoId(), check('productItemId').isMongoId()], ProductItemController.get)
+productItemRoutes.post('/products/:productId/items', [
+  check('quantity').exists().isNumeric(),
+  check('price').exists().isNumeric(),
+  check('expiration').exists(),
+  check('productId').isMongoId()
+], ProductItemController.store)
 
 export default productItemRoutes
