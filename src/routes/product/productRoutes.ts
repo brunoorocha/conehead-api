dc7187dca0809f434edd75d7407f77f764340103
Bruno Rocha <mail.brunorocha@gmail.com>
2019-11-04 08:35:57 -0300

    Update productRoutes.ts
    Removing route protection in GET /products

--- src/routes/product/productRoutes.ts
+++ src/routes/product/productRoutes.ts
@@ -1,10 +1,9 @@
 import express from 'express'
 import ProductController from '../../controllers/ProductController'
-import passport from 'passport'
 import { check } from 'express-validator'
 
 const productRoutes = express.Router()
-productRoutes.get('/products', passport.authenticate('jwt', { session: false }), ProductController.index)
+productRoutes.get('/products', ProductController.index)
 productRoutes.post('/products', [check('name').isLength({ min: 3 }), check('measurementId').isMongoId()], ProductController.store)
 productRoutes.get('/products/:productId', [check('productId').isMongoId()], ProductController.get)
 productRoutes.delete('/products/:productId', [check('productId').isMongoId()], ProductController.remove)
