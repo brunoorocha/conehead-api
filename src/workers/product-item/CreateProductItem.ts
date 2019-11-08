37ee1615c1e9816c59dc67efa75999356a1ccc9d
Bruno Rocha <mail.brunorocha@gmail.com>
2019-11-08 11:13:35 -0300

    Returning null expiration date for product items without one

--- src/workers/product-item/CreateProductItem.ts
+++ src/workers/product-item/CreateProductItem.ts
@@ -4,7 +4,7 @@ import Product from '../../models/Product'
 
 const createProductItem = async (quantity: number, price: number, expiration: string, productId: string, userId: string, productItemStore: ProductItemStore): Promise<ProductItem> => {
   const product = new Product(productId, null, null)
-  const expirationDate = expiration ? new Date(expiration) : undefined
+  const expirationDate = expiration ? new Date(expiration) : null
   const productItem = new ProductItem(null, quantity, price, expirationDate, new Date(), product)
   const storedProductItem = await productItemStore.save(productItem, userId)
   return storedProductItem
