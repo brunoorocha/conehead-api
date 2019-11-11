c473b4bd0cdd6c536c756704c9b1989813d49494
Bruno Rocha <mail.brunorocha@gmail.com>
2019-11-11 08:22:09 -0300

    Removing home notifications

--- src/routes/notifications/NotificationRoutes.ts
+++ src/routes/notifications/NotificationRoutes.ts
@@ -3,9 +3,9 @@ import express from 'express'
 const notificationRoutes = express.Router()
 notificationRoutes.get('/notifications', (_, res) => {
   return res.json([
-    { emoji: '🚨', message: 'O molho de tomate está quase vencendo.', type: 'alert' },
-    { emoji: '⚠️', message: 'Você está quase sem pão bola.', type: 'warning' },
-    { emoji: '🎅🏻', message: 'Jingle Bell! O Natal está chegando. Que tal revisar o estoque para ver se é necessário repor alguns items e atender os seus clientes sem surpresas.', type: 'normal' }
+    // { emoji: '🚨', message: 'O molho de tomate está quase vencendo.', type: 'alert' },
+    // { emoji: '⚠️', message: 'Você está quase sem pão bola.', type: 'warning' },
+    // { emoji: '🎅🏻', message: 'Jingle Bell! O Natal está chegando. Que tal revisar o estoque para ver se é necessário repor alguns items e atender os seus clientes sem surpresas.', type: 'normal' }
   ])
 })
 
