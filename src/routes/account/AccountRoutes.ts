9d58c5f1377195a8c34c42b329c36e8cedea3830
Bruno Rocha <mail.brunorocha@gmail.com>
2019-11-21 09:39:24 -0300

    Validating if the fields in account creation aren't empty

--- src/routes/account/AccountRoutes.ts
+++ src/routes/account/AccountRoutes.ts
@@ -5,10 +5,10 @@ import AccountController from '../../controllers/AccountController'
 
 const accountRoutes = Router()
 accountRoutes.post('/accounts', [
-  check('name').exists().isString(),
+  check('name').exists().isString().isLength({ min: 3 }),
   check('email').exists().isEmail(),
   check('password').isLength({ min: 6 }),
-  check('passwordConfirmation').exists().custom(passwordConfirmationCheck)
+  check('passwordConfirmation').exists().isLength({ min: 6 }).custom(passwordConfirmationCheck)
 ], AccountController.store)
 
 export default accountRoutes
